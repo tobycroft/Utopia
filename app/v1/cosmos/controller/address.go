da35edc34f7bc61c508704db99c77230a57f1335
Tuuz <2539@aerofsx.com>
2020-02-14 21:38:29 +0800

    Tuuz

--- app/v1/cosmos/controller/address.go
+++ app/v1/cosmos/controller/address.go
@@ -11,6 +11,8 @@ func AddressController(route *gin.RouterGroup) {
 	route.Any("/", index)
 	route.Any("/to", to)
 	route.Any("/from", from)
+	route.Any("/to_clean", to_clean)
+	route.Any("/from_clean", from_clean)
 }
 
 func to(c *gin.Context) {
@@ -32,6 +34,31 @@ func to(c *gin.Context) {
 	c.JSON(200, to)
 }
 
+func to_clean(c *gin.Context) {
+	address, had := c.GetPostForm("address")
+	if had == false {
+		c.JSON(200, RET.Fail(404, "address"))
+		c.Abort()
+		return
+	}
+	limit, had := c.GetPostForm("limit")
+	if had == false {
+		limit = "30"
+	}
+	page, had := c.GetPostForm("page")
+	if had == false {
+		page = "1"
+	}
+	to := BlocksModel.Api_select_byToAddress(address, Calc.Any2Int(limit), Calc.Any2Int(page))
+	if len(to) > 0 {
+		for i, data := range to {
+			delete(data, "raw")
+			to[i] = data
+		}
+	}
+	c.JSON(200, to)
+}
+
 func from(c *gin.Context) {
 	address, had := c.GetPostForm("address")
 	if had == false {
@@ -48,5 +75,30 @@ func from(c *gin.Context) {
 		page = "1"
 	}
 	from := BlocksModel.Api_select_byFromAddress(address, Calc.Any2Int(limit), Calc.Any2Int(page))
+	c.JSON(200, from)
+}
+
+func from_clean(c *gin.Context) {
+	address, had := c.GetPostForm("address")
+	if had == false {
+		c.JSON(200, RET.Fail(404, "address"))
+		c.Abort()
+		return
+	}
+	limit, had := c.GetPostForm("limit")
+	if had == false {
+		limit = "30"
+	}
+	page, had := c.GetPostForm("page")
+	if had == false {
+		page = "1"
+	}
+	from := BlocksModel.Api_select_byFromAddress(address, Calc.Any2Int(limit), Calc.Any2Int(page))
+	if len(from) > 0 {
+		for i, data := range from {
+			delete(data, "raw")
+			from[i] = data
+		}
+	}
 	c.JSON(200, from)
 }
